00d6d3bfe7bb34d17dc268a26850867769699aaf
yss525 <1728411191@qq.com>
2020-08-31 22:04:47 +0800

    yes

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
 git push -f git@github.com:yss525/yss525.github.io.git master
+# git push -f git@gitee.com:ygg520lys/ygg520lys.gitee.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
